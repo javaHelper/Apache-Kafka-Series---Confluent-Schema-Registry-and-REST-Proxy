6a53d8bbcce552572d8f2cdffa58944c15b92771
Prateek <54174687+javaHelper@users.noreply.github.com>
2022-08-22 23:39:19 +0530

    Update 0-create-topics.sh

--- 4-rest-proxy/0-create-topics.sh
+++ 4-rest-proxy/0-create-topics.sh
@@ -1,3 +1,3 @@
-kafka-topics --create --zookeeper localhost:2181 --topic rest-binary --replication-factor 1 --partitions 1
-kafka-topics --create --zookeeper localhost:2181 --topic rest-json --replication-factor 1 --partitions 1
-kafka-topics --create --zookeeper localhost:2181 --topic rest-avro --replication-factor 1 --partitions 1
+kafka-topics --create --bootstrap-server localhost:9092 --topic rest-binary --replication-factor 1 --partitions 1
+kafka-topics --create --bootstrap-server localhost:9092 --topic rest-json --replication-factor 1 --partitions 1
+kafka-topics --create --bootstrap-server localhost:9092 --topic rest-avro --replication-factor 1 --partitions 1
